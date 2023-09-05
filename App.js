33e5da9d0953a224b5824502c9e7358b431c53ee
username895 <89336140+username895@users.noreply.github.com>
2023-09-05 15:05:54 +0300

    Upload

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styles from './StyleSheet';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+import * as Location from 'expo-location'; 
 
 export default function App() {
+  const apiKey = '17ce59ebe9bfc2a094ca4ab9d2389caa';
+
+  let [latitude, setLatitude] = useState(0)
+  let [longitude, setLongitude] = useState(0)
+  let [weatherImages, setWeatherImages] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Запрос разрешения на геолокацию
+        const locationPermission = await Location.requestForegroundPermissionsAsync();
+        if (locationPermission.status === 'granted') {
+          // Получение текущей позиции
+          const position = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          console.log(latitude, longitude);
+  
+          // Здесь вы можете установить значение в состояние
+          setLatitude(latitude);
+          setLongitude(longitude);
+  
+          // Запрос погодных данных
+          const response = await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+          );
+          setWeatherImages((weatherImages) => [`http://openweathermap.org/img/w/${response.data.weather[0].icon}`])
+          console.log(response.data);
+          console.log(weatherImages);
+        } else {
+          console.log('Location permission not granted');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+  
+    fetchData(); // Вызываем функцию fetchData внутри useEffect
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="dark-content" /> */}
+      <Text style={{ color: '#000' }}>Test text</Text>
+      <Image style={styles.weatherImage} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png',}}/>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+        style={
+          {
+            maxWidth: 300,
+            width: 100,
+            height: 100
+          }
+        }
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ StyleSheet.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Занимает всю доступную высоту
+    backgroundColor: 'white', 
+    borderWidth: 2,
+    borderColor: 'red',
+    marginHorizontal: 10,
+    padding: 10,
+  },
+  statusBar: {
+    borderWidth: 2,
+    borderColor: 'red',
+    margin: 10,
+    padding: 10,
+  },
+  weatherImage: {
+    width: 100,
+    height: 100,
+    
+  }
+  // text: {
+  //   fontSize: 20,
+  //   fontWeight: 'bold',
+  //   color: 'white',
+  // },
+});
+
+export default styles;
